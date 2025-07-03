014b8d83b43d66a28bbd68736f278f12bcff4f1a
iru <study.iru@gmail.com>
2025-07-03 20:46:01 +0900

    js 를 ts 로 변경

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,37 @@
+declare module '*.svg' {
+    import React = require('react');
+
+    export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+    const src: string;
+    export default src;
+}
+
+declare module '*.png' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.jpg' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.jpeg' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.gif' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.bmp' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.tiff' {
+    const value: string;
+    export default value;
+}
--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,5 +1,7 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
+import type { Metric } from 'web-vitals';
+
+const reportWebVitals = (onPerfEntry?: (metric: Metric) => void) => {
+  if (onPerfEntry && typeof onPerfEntry === 'function') {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
